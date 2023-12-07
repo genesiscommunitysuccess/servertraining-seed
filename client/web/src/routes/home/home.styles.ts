48b1611c37656cecf1c8e17673a4a52f65936e6b
dbarros1979 <daniel.barros@genesis.global>
2023-12-07 17:24:32 +0000

    Fixing client issues

--- client/web/src/routes/home/home.styles.ts
+++ client/web/src/routes/home/home.styles.ts
@@ -3,7 +3,6 @@ import {mixinScreen} from '../../styles';
 
 export const HomeStyles = css`
   :host {
-    ${mixinScreen('flex')}
     align-items: center;
     justify-content: center;
     flex-direction: column;
@@ -11,14 +10,14 @@ export const HomeStyles = css`
   }
 
   .split-layout {
-      display: flex;
       flex-direction: column;
       flex: 1;
       width: 100%;
+      height: 100%;
   }
 
   .top-layout {
-  height: 90%;
+  height: 45%;
   flex-direction: row;
   }
 
