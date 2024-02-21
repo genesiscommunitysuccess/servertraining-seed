f14711478911864c49212366bcdc6a7200140cb4
GuilhermeGuerraMarti <guilherme.guerra@genesis.global>
2024-02-21 16:32:37 +0000

    module 4 - reqrep

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-reqrep.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-reqrep.kts
@@ -1,17 +1,5 @@
-import global.genesis.gen.config.fields.Fields
-
-/**
- * System              : Genesis Business Library
- * Sub-System          : multi-pro-code-test Configuration
- * Version             : 1.0
- * Copyright           : (c) Genesis
- * Date                : 2022-03-18
- * Function : Provide request reply config for multi-pro-code-test.
- *
- * Modification History
- */
 requestReplies {
-    requestReply("COUNTERPARTY_DETAILS",COUNTERPARTY){
+    requestReply("TRADE_DETAILS",TRADE){
         request {
             COUNTERPARTY_ID withTransformation { type, _ ->
                 if (type == "2") {
@@ -20,6 +8,14 @@ requestReplies {
                     type
                 }
             }
+        }
+
+        reply {
+            TRADE_ID
+            COUNTERPARTY_ID
+            INSTRUMENT_ID
+            QUANTITY
+            PRICE
         }
     }
 }
