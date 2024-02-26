de2b27d017969e789bb1303e3ea7babe9f6b5218
GuilhermeGuerraMarti <guilherme.guerra@genesis.global>
2024-02-26 17:51:25 +0000

    Module 5 - Lesson 2 - webhandlers

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-streamer.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-streamer.kts
@@ -1,7 +1,11 @@
 streams {
     stream("TRADE_AUDIT_STREAM", TRADE_AUDIT.BY_TIMESTAMP){
+        where { trades->
+            trades.quantity!! > 100
+        }
         fields{
             TRADE.TRADE_ID
+
         }
     }
 }
--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-web-handler.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-web-handler.kts
@@ -1,29 +1,30 @@
 import global.genesis.commons.standards.GenesisPaths
 import global.genesis.message.core.HttpStatusCode
 import java.nio.file.*
-import java.io.File
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 webHandlers("BASE-PATH"){
-
-
-    multipartEndpoint("test") {
-        config{
+    multipartEndpoint("FILE_UPLOAD",status=HttpStatusCode.Forbidden){
+        val fileUploadFolder = "traning/files"
+        config {
             multiPart {
-                maxFileSize = 10_000_000
+                maxFileSize - 10_000_000
                 useDisk = true
-                baseDir = "GUI_TEST/files"
+                baseDir = fileUploadFolder
             }
         }
-
+        val savedFolder = Paths.get(GenesisPaths.runtime() + "/" + fileUploadFolder)
         handleRequest {
             body.fileUploads.forEach{
-                val pasta = Paths.get("${GenesisPaths.genesisHome()}/runtime/GUI_TEST/files")
-                it.copyTo(pasta.resolve(it.fileName))
-                "Deu Boa"
-                LOG.info("DEU BOA")
+                it.copyTo(savedFolder.resolve(it.fileName))
             }
         }
     }
+    endpoint<Trade,Trade>(GET,"TEST"){
+        handleRequest{
+            val trade = db.insert(body).record
+            triggerEvent("TRADE_AUDIT_STREAM",trade)
+            trade
+        }
+    }
 }
+
