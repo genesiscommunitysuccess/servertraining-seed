109b6f52b62a1fd37bdf4b95551761064bb538b1
GuilhermeGuerraMarti <guilherme.guerra@genesis.global>
2024-02-27 10:23:38 +0000

    Module 5 - Lesson 2 - webhandlers

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-web-handler.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-web-handler.kts
@@ -3,26 +3,11 @@ import global.genesis.message.core.HttpStatusCode
 import java.nio.file.*
 
 webHandlers("BASE-PATH"){
-    multipartEndpoint("FILE_UPLOAD",status=HttpStatusCode.Forbidden){
-        val fileUploadFolder = "traning/files"
-        config {
-            multiPart {
-                maxFileSize - 10_000_000
-                useDisk = true
-                baseDir = fileUploadFolder
-            }
-        }
-        val savedFolder = Paths.get(GenesisPaths.runtime() + "/" + fileUploadFolder)
-        handleRequest {
-            body.fileUploads.forEach{
-                it.copyTo(savedFolder.resolve(it.fileName))
-            }
-        }
-    }
-    endpoint<Trade,Trade>(GET,"TEST"){
+
+    endpoint<Trade,Trade>(GET,"TEST", HttpStatusCode.Not){
         handleRequest{
             val trade = db.insert(body).record
-            triggerEvent("TRADE_AUDIT_STREAM",trade)
+            triggerEvent("TRADE_AUDIT_STREAM")
             trade
         }
     }
