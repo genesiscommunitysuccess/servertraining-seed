4ea0eef1d5b7cbc28cfa7d52c2c49229e7d91581
GuilhermeGuerraMarti <guilherme.guerra@genesis.global>
2024-02-27 17:39:59 +0000

    Module 5 - Lesson 4 - Pipeline. Exercise 5.4

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-data-pipeline.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-data-pipeline.kts
@@ -1,19 +1,25 @@
 import global.genesis.commons.standards.GenesisPaths
-import global.genesis.gen.config.tables.COUNTERPARTY.
+import global.genesis.data.ingest.source.SinkOperations
+import global.genesis.data.ingest.transform.SinkOperation
+import global.genesis.gen.config.tables.COUNTERPARTY
+import global.genesis.gen.config.tables.COUNTERPARTY.COUNTERPARTY_ID
+import global.genesis.gen.config.tables.COUNTERPARTY.COUNTERPARTY_NAME
+import global.genesis.gen.config.tables.COUNTERPARTY.COUNTERPARTY_LEI
 
 pipelines{
     csvSource("cp-pipeline"){
-        location = "file:${GenesisPaths.runtime()}/fileIngress?fileName=cp-pipeline.csv"
+        location = "file:${GenesisPaths.runtime()}/fileIngress?fileName=cp-pipeline.csv&delete=true"
+        errorStrategy = AllOrNothing()
 
-        map("mapper-name",COUNTERPARTY){
+        map("mapper-name", COUNTERPARTY){
+            COUNTERPARTY_ID{
+                property = "Id"
+            }
             COUNTERPARTY_NAME{
                 property = "Counterparty Name"
             }
             COUNTERPARTY_LEI{
                 property = "Counterparty Lei"
-            }
-            COUNTERPARTY_ID{
-                property = "Id"
             }
         }
     }
